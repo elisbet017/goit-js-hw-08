2e0cb0a518df1ef09f01cc6e969635ab7ea76776
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-05-03 16:12:04 +0200

    Update files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Vimeo from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
 
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onTimeUpdate);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+// function onTimeUpdate() {
+//   console.log('Play');
+// }
+// function onSavedCurrentTime() {
+//   localStorage.setItem('videoplayer-current-time');
+// }
