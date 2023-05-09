5bd24f863fcc28e56027c9d58018cf16eb3f7f2f
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-05-09 13:21:13 +0200

    Fix feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,42 +3,39 @@ import throttle from 'lodash.throttle';
 const INPUT_VALUE_KEY = 'feedback-form-state';
 const refs = {
   form: document.querySelector('.feedback-form'),
-  email: document.querySelector('[name="email"]'),
-  message: document.querySelector('[name="message"]'),
 };
 
 refs.form.addEventListener('input', throttle(onSaveInputValue, 500));
 refs.form.addEventListener('submit', onSubmitForm);
 
+let dataForm = {};
+
 function onSaveInputValue(e) {
-  localStorage.setItem(
-    INPUT_VALUE_KEY,
-    JSON.stringify({
-      email: refs.email.value,
-      message: refs.message.value,
-    })
-  );
+  dataForm[e.target.name] = e.target.value.trim();
+  localStorage.setItem(INPUT_VALUE_KEY, JSON.stringify(dataForm));
 }
 
 function onSubmitForm(e) {
   e.preventDefault();
-  const user = {
-    email: refs.email.value,
-    message: refs.message.value,
-  };
-  if (refs.email.value !== '' && refs.message.value !== '') {
-    console.log(user);
-  } else {
-    alert('Заповніть, будь-ласка, всі поля.');
-  }
-  refs.form.reset();
+
+  console.log(dataForm);
+  dataForm = {};
+
+  e.target.reset();
   localStorage.removeItem(INPUT_VALUE_KEY);
 }
 
-const objectInputsValues = JSON.parse(localStorage.getItem(INPUT_VALUE_KEY));
-
 function updateInput() {
-  refs.email.value = objectInputsValues ? objectInputsValues.email : '';
-  refs.message.value = objectInputsValues ? objectInputsValues.message : '';
+  try {
+    const data = localStorage.getItem(INPUT_VALUE_KEY);
+    if (!data) return;
+    dataForm = JSON.parse(data);
+    Object.entries(dataForm).forEach(([key, value]) => {
+      refs.form.elements[key].value = value;
+    })
+  } catch (error) {
+    console.log(error.message);
+  }
 }
+
 updateInput();
