703e82b1a3f9a4038106988d49df0be5f4562014
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-05-14 09:18:21 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-// jhjh
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onStartSwitchColor);
+refs.btnStop.addEventListener('click', onStopSwitchColor);
+
+function onStartSwitchColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000)
+  refs.btnStart.setAttribute('disabled', '');
+}
+
+function onStopSwitchColor() {
+  clearInterval(timerId);
+  refs.btnStart.removeAttribute('disabled', '');
+
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
