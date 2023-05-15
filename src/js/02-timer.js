a94bc04744a252797d20315cd2f47a7bb9c1999b
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-05-15 12:15:07 +0200

    Add styles and update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,7 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 refs = {
   dataInput: document.querySelector('[id="datetime-picker"]'),
   startBtn: document.querySelector('[data-start]'),
@@ -22,54 +24,53 @@ const options = {
 };
 
 function onChooseDate(selectedDates) {
-  const currentDate = new Date();
-  const chooseDate = new Date(selectedDates[0]);
-  if (currentDate.getTime() > chooseDate.getTime()) {
-    alert('Please choose a date in the future');
-    refs.startBtn.setAttribute('disabled', '');
+  const chooseDate = new Date(selectedDates[0]).getTime();
+  console.log(chooseDate);
+  if (Date.now() > chooseDate) {
+    Notify.failure('Please choose a date in the future');
     return;
   }
   refs.startBtn.removeAttribute('disabled', '');
   refs.startBtn.addEventListener('click', onStartBtnClick);
 
   function onStartBtnClick() {
+    showTime();
     const intervalId = setInterval(showTime, 1000);
-    if (
-      new Date().getTime() === chooseDate.getTime() ||
-      new Date().getTime() > chooseDate.getTime()
-    ) {
-      clearInterval(intervalId);
+    function showTime() {
+      if (Date.now() >= chooseDate) {
+        clearInterval(intervalId);
+        return;
+      }
+      const timeLeft = chooseDate - Date.now();
+      function convertMs(ms) {
+        // Number of milliseconds per unit of time
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+        // Remaining days
+        const days = addLeadingZero(Math.floor(ms / day));
+        // Remaining hours
+        const hours = addLeadingZero(Math.floor((ms % day) / hour));
+        // Remaining minutes
+        const minutes = addLeadingZero(
+          Math.floor(((ms % day) % hour) / minute)
+        );
+        // Remaining seconds
+        const seconds = addLeadingZero(
+          Math.floor((((ms % day) % hour) % minute) / second)
+        );
+
+        return { days, hours, minutes, seconds };
+      }
+      const time = convertMs(timeLeft);
+
+      refs.days.textContent = time.days;
+      refs.hours.textContent = time.hours;
+      refs.minutes.textContent = time.minutes;
+      refs.seconds.textContent = time.seconds;
     }
-  }
-  function showTime() {
-    const dateNow = new Date();
-    const timeLeft = chooseDate.getTime() - dateNow.getTime();
-    function convertMs(ms) {
-      // Number of milliseconds per unit of time
-      const second = 1000;
-      const minute = second * 60;
-      const hour = minute * 60;
-      const day = hour * 24;
-
-      // Remaining days
-      const days = addLeadingZero(Math.floor(ms / day));
-      // Remaining hours
-      const hours = addLeadingZero(Math.floor((ms % day) / hour));
-      // Remaining minutes
-      const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-      // Remaining seconds
-      const seconds = addLeadingZero(
-        Math.floor((((ms % day) % hour) % minute) / second)
-      );
-
-      return { days, hours, minutes, seconds };
-    }
-    const time = convertMs(timeLeft);
-
-    refs.days.textContent = time.days;
-    refs.hours.textContent = time.hours;
-    refs.minutes.textContent = time.minutes;
-    refs.seconds.textContent = time.seconds;
   }
 }
 
