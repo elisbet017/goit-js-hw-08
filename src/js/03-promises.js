8aba7aa8d77ed661764ecfdb5a901979e6830bdd
Shchyrova Yelyzaveta <lissa02102005@gmail.com>
2023-05-16 08:24:50 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,24 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const refs = {
+  submitBtn: document.querySelector('[type="submit"]'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+// refs.submitBtn.addEventListener('submit', createPromise);
+
+refs.submitBtn.addEventListener('submit', test);
+
+function test(e) {
+  e.preventDefault();
+  console.log(refs.delay.value);
+}
+function createPromise() {
+  // console.log(refs.step.value);
+  // console.log(refs.amount.value);
+  // const shouldResolve = Math.random() > 0.3;
+  // if (shouldResolve) {
+  //   // Fulfill
+  // } else {
+  //   // Reject
+  // }
 }
