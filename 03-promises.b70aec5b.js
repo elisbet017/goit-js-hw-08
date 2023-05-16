e6be133b3b2e6a5e4e379b2c7205a2caedb768bc
elisbet017 <120185827+elisbet017@users.noreply.github.com>
2023-05-16 06:25:23 +0000

    Deploying to gh-pages from @ elisbet017/goit-js-hw-09@8aba7aa8d77ed661764ecfdb5a901979e6830bdd 🚀

---
+++ 03-promises.b70aec5b.js
@@ -0,0 +1,2 @@
+const e={submitBtn:document.querySelector('[type="submit"]'),delay:document.querySelector('[name="delay"]'),step:document.querySelector('[name="step"]'),amount:document.querySelector('[name="amount"]')};e.submitBtn.addEventListener("submit",(function(t){t.preventDefault(),console.log(e.delay.value)}));
+//# sourceMappingURL=03-promises.b70aec5b.js.map
---
+++ 03-promises.b89e5e15.js
@@ -0,0 +1,2 @@
+!function(){const e={submitBtn:document.querySelector('[type="submit"]'),delay:document.querySelector('[name="delay"]'),step:document.querySelector('[name="step"]'),amount:document.querySelector('[name="amount"]')};e.submitBtn.addEventListener("submit",(function(t){t.preventDefault(),console.log(e.delay.value)}))}();
+//# sourceMappingURL=03-promises.b89e5e15.js.map
